f3b0d3a36db058c015f46478f59a04ff22d7a366
brany suero <brany.suero@coface.com>
2018-08-09 14:12:42 -0400

    add the material package and a new dependency in package.json

--- mean/mean-code/src/app/app.module.ts
+++ mean/mean-code/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
 
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
